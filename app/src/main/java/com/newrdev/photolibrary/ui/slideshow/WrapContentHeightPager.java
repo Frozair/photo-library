783b1ecd471cf33a8c07ca9cb77fe70cc30f39e2
Rudolph Newball <newrdev@gmail.com>
2016-10-05 19:40:47 -0500

    Class that extends pager and sets the first child height as the height to wrap.

---
+++ app/src/main/java/com/newrdev/photolibrary/ui/slideshow/WrapContentHeightPager.java
@@ -0,0 +1,42 @@
+package com.newrdev.photolibrary.ui.slideshow;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+/**
+ * Created by newrdev on 10/5/16.
+ */
+
+public class WrapContentHeightPager extends ViewPager {
+
+    private int mCurrentPagePosition = 0;
+
+    public WrapContentHeightPager(Context context) {
+        super(context);
+    }
+
+    public WrapContentHeightPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        try {
+            View child = getChildAt(mCurrentPagePosition);
+            if (child != null) {
+                child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+                int h = child.getMeasuredHeight();
+                heightMeasureSpec = MeasureSpec.makeMeasureSpec(h, MeasureSpec.EXACTLY);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    public void reMeasureCurrentPage(int position) {
+        mCurrentPagePosition = position;
+        requestLayout();
+    }
+}
